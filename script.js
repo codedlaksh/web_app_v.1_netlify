ad2735445799c7af6ac7eb42b067eadeded02af2
LAKSHMI GOPI <69383937+codedlaksh@users.noreply.github.com>
2024-04-08 22:24:51 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+// document.getElementById('jogDown').addEventListener('click', function() {
+//     // logic for Jog Down button
+// });
+
+// document.getElementById('jogUp').addEventListener('click', function() {
+//     // logic for Jog Up button
+// });
+
+// document.querySelectorAll('.teach').forEach(button => {
+//     button.addEventListener('click', function() {
+//         // logic for Teach buttons
+//     });
+// });
+// document.addEventListener('DOMContentLoaded', function() {
+// document.querySelector('start_session').addEventListener('click', function() {
+//     var img = this.querySelector('img');
+//     if (img.src.endsWith('Stop_Pause.png')) {
+//         img.src = 'imageB.png'; // replace with the path to the second image
+//     } else {
+//         img.src = 'Start_Resume.png';
+//     }
+// });
+// });
